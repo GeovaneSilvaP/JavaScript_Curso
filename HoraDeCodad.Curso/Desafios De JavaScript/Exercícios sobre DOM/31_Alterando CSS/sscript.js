aedf765f6d81a19f177b4f2e7bb5d045f2cddafe
GeovaneSilvaP <geovanesil45sil@gmail.com>
2025-10-12 07:28:43 -0300

    dom

---
+++ HoraDeCodad.Curso/Desafios De JavaScript/Exercícios sobre DOM/31_Alterando CSS/sscript.js
@@ -0,0 +1,13 @@
+function mudarCor() {
+    const red = document.getElementById('red');
+    const blue = document.getElementById('blue');
+    const green = document.getElementById('green');
+    const yellow = document.getElementById('yellow');
+
+    red.style.color = "red";
+    blue.style.color = "blue";
+    green.style.color = "green";
+    yellow.style.color = "yellow";
+}
+
+mudarCor();
